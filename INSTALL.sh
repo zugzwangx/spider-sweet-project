5b8916b8d666788643e6dceb10096fbc1a5a60eb
D <zugzwang@GROOL-PC.local>
2018-08-23 20:03:42 -0700

    Updated INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Install script will copy the SpiderSweet libraries into
+# the user's Arduino library, located at
+# ${HOME}/Arduino/libraries
+
 cd libraries
 [ -d ${HOME}/Arduino/libraries ] &&
 /bin/ls | while read folders; do
